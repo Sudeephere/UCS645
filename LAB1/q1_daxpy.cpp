8b4541321fb41df2265c78a160110d2cf4b6016d
Sudeephere <ssudeep60_be24@thapar.edu>
2026-02-02 13:55:57 +0530

    Update q1_daxpy.cpp

--- LAB1/q1_daxpy.cpp
+++ LAB1/q1_daxpy.cpp
@@ -3,37 +3,35 @@
 using namespace std;
 
 int main() {
-    const int N = 65536;
+    int N = 65536;
+    double X[N], Y[N];
     double a = 2.5;
-
-    double *X = new double[N];
-    double *Y = new double[N];
+    double t1, t2, seq_time;
 
     for (int i = 0; i < N; i++) {
         X[i] = 1.0;
         Y[i] = 2.0;
     }
 
-    double t1 = omp_get_wtime();
+    // Sequential
+    t1 = omp_get_wtime();
     for (int i = 0; i < N; i++) {
         X[i] = a * X[i] + Y[i];
     }
-    double t2 = omp_get_wtime();
+    t2 = omp_get_wtime();
+    seq_time = t2 - t1;
 
-    double seq_time = t2 - t1;
+    cout << "Sequential Time = " << seq_time << " seconds\n";
 
-    cout << "Sequential Time = " << seq_time << " seconds\n\n";
-
+    // Parallel
     for (int threads = 2; threads <= 12; threads++) {
-
-        for (int i = 0; i < N; i++) {
-            X[i] = 1.0;
-        }
-
         omp_set_num_threads(threads);
 
+        for (int i = 0; i < N; i++)
+            X[i] = 1.0;
+
         t1 = omp_get_wtime();
-#pragma omp parallel for
+        #pragma omp parallel for
         for (int i = 0; i < N; i++) {
             X[i] = a * X[i] + Y[i];
         }
@@ -43,12 +41,9 @@ int main() {
         double speedup = seq_time / par_time;
 
         cout << "Threads = " << threads
-             << "   Time = " << par_time
-             << "   Speedup = " << speedup << endl;
+             << " Time = " << par_time
+             << " Speedup = " << speedup << endl;
     }
-
-    delete[] X;
-    delete[] Y;
 
     return 0;
 }
