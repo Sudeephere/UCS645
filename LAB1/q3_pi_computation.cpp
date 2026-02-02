fb37326974477542b2811d9cf5a389d58b2bb5ee
Sudeephere <ssudeep60_be24@thapar.edu>
2026-02-02 13:57:25 +0530

    Update q3_pi_computation.cpp

--- LAB1/q3_pi_computation.cpp
+++ LAB1/q3_pi_computation.cpp
@@ -3,40 +3,44 @@
 using namespace std;
 
 int main() {
-    const long steps = 1000000;
-    double step = 1.0 / steps;
-    double sum = 0.0;
+    long num_steps = 100000;
+    double step = 1.0 / num_steps;
+    double sum = 0.0, pi;
+    double t1, t2, seq_time;
 
-    double t1 = omp_get_wtime();
-    for (long i = 0; i < steps; i++) {
+    // Sequential
+    t1 = omp_get_wtime();
+    for (long i = 0; i < num_steps; i++) {
         double x = (i + 0.5) * step;
         sum += 4.0 / (1.0 + x * x);
     }
-    double pi = step * sum;
-    double t2 = omp_get_wtime();
-    double seq_time = t2 - t1;
-    cout << "Sequential:\n";
-    cout << "Pi = " << pi << endl;
-    cout << "Time = " << seq_time << " seconds\n\n";
+    pi = step * sum;
+    t2 = omp_get_wtime();
 
-    cout << "Parallel:\n";
-    for (int threads = 2; threads <= 8; threads++) {
-        sum = 0.0;
+    seq_time = t2 - t1;
+    cout << "Sequential Pi = " << pi << endl;
+    cout << "Time = " << seq_time << endl;
+
+    // Parallel
+    cout << "\nParallel:\n";
+    for (int threads = 2; threads <= 10; threads++) {
         omp_set_num_threads(threads);
+        sum = 0.0;
+
         t1 = omp_get_wtime();
-#pragma omp parallel for reduction(+:sum)
-        for (long i = 0; i < steps; i++) {
+        #pragma omp parallel for reduction(+:sum)
+        for (long i = 0; i < num_steps; i++) {
             double x = (i + 0.5) * step;
             sum += 4.0 / (1.0 + x * x);
         }
         pi = step * sum;
         t2 = omp_get_wtime();
+
         double par_time = t2 - t1;
-        double speedup = seq_time / par_time;
-        cout << "Threads = " << threads
-             << "   Pi = " << pi
-             << "   Time = " << par_time
-             << "   Speedup = " << speedup << endl;
+        cout << "Threads=" << threads
+             << " Pi=" << pi
+             << " Time=" << par_time
+             << " Speedup=" << seq_time / par_time << endl;
     }
 
     return 0;
